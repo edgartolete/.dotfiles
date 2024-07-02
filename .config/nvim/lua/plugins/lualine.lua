76a27262fc35978b377eb0ed403d69ab3031e55c
Edgar Tolete <edgartolete@outlook.com>
2024-07-02 19:31:21 +0800

    disable lualine full path

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,35 +7,34 @@ return {
       --   return vim.fn.expand('%:p')
       -- end
 
-      local function filepath()
-        local file_path = vim.fn.expand('%:p')
-        local project_root = vim.fn.getcwd()
-        return file_path:sub(#project_root + 2)
-      end
-
-
-      -- # TODO: Currently not working to only use file name for java projects
-      local current_buffer = vim.api.nvim_get_current_buf()
-      -- local file_name = vim.api.nvim_buf_get_name(0)
-
-      local filename = vim.api.nvim_buf_get_name(current_buffer)
-      -- local file_extension = file_name:match("^.+(%..+)$")
-
-
-      local buff;
-
-      if string.sub(filename, -4) == ".java" then
-        -- print("The current file is a Java file.")
-        buff = {
-          sections =  { lualine_c = { "filetype"}}
-        }
-      else
-        -- print("The current file is not a Java file.")
-        buff = { sections = { lualine_c = { filepath }}}
-      end
-
-      print(buff)
-
-      require('lualine').setup(buff)
+      -- local function filepath()
+      --   local file_path = vim.fn.expand('%:p')
+      --   local project_root = vim.fn.getcwd()
+      --   return file_path:sub(#project_root + 2)
+      -- end
+      --
+      --
+      -- -- # TODO: Currently not working to only use file name for java projects
+      -- local current_buffer = vim.api.nvim_get_current_buf()
+      -- -- local file_name = vim.api.nvim_buf_get_name(0)
+      --
+      -- local filename = vim.api.nvim_buf_get_name(current_buffer)
+      -- -- local file_extension = file_name:match("^.+(%..+)$")
+      --
+      --
+      -- local buff;
+      --
+      -- if string.sub(filename, -4) == ".java" then
+      --   -- print("The current file is a Java file.")
+      --   buff = {
+      --     sections =  { lualine_c = { "filetype"}}
+      --   }
+      -- else
+      --   -- print("The current file is not a Java file.")
+      --   buff = { sections = { lualine_c = { filepath }}}
+      -- end
+      --
+      -- require('lualine').setup(buff)
+      require('lualine').setup()
     end,
 }
