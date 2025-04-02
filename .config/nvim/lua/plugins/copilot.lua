143396837f3a351c5a1040ff4ec61c777fd1e7fa
Edgar Tolete <edgartolete@outlook.com>
2025-04-02 11:42:23 +0800

    updated accept for copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -25,7 +25,7 @@ return {
         hide_during_completion = true,
         debounce = 75,
         keymap = {
-          accept = "<Space>",
+          accept = "<M-CR>",
           accept_word = false,
           accept_line = false,
           next = "<M-n>",
